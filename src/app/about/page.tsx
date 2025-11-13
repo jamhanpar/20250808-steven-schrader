56d29862e48aa02b2bb8d7afd7622516b9ce5372
Jamhanpar <james.hansung.park@gmail.com>
2025-11-12 22:35:26 -0500

    resolve build errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import MosaicGallery, { MosaicItem } from "app/components/mosaic/Mosaic";
 import Section from "app/components/section/Section";
 import authorData from "app/data/about-data.json";
-import { mosaicData } from "app/data/about-mosaic-data.json";
+import mosaicDataJson from "app/data/about-mosaic-data.json";
 
 export default function BioPage() {
   return (
@@ -9,7 +9,10 @@ export default function BioPage() {
       <Section classname="flex flex-col-reverse gap-8 lg:flex-row">
         {/* Left: Profile and creative image grid */}
         <div className="flex flex-col gap-6 items-center lg:w-1/2 lg:pt-[60px]">
-          <MosaicGallery items={mosaicData as MosaicItem[]} maxColumns={3} />
+          <MosaicGallery
+            items={mosaicDataJson.mosaicData as MosaicItem[]}
+            maxColumns={3}
+          />
         </div>
         {/* Right: Bio text */}
         <div className="flex flex-col gap-4 items-center lg:w-1/2 lg:items-start">
